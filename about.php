7cb1ffdbe1c8708656f5973f298c56c4c1841b8b
Mrowamohmed <121772686+Mrowamohmed@users.noreply.github.com>
2023-01-02 00:19:16 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,82 @@
+<html>
+    <head>
+        <title> login</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css\par.css">
+    </head>
+<body>
+    <div class="log" >
+        <img style="width: 100px;float: right;margin-right: 20px;" src="img/img2.png"  > 
+      
+    </div>
+     
+    <nav class="menu">
+     <ul>
+      <li><a href="http://localhost/market/radi.php"><h3>الرئيسية</h3></a></li>    
+      <li><a href="http://localhost/market/gallery.php"><h3>الاقسام</h3></a></li> 
+      <li><a href="http://localhost/market/help.php"><h3>المساعدة</h3></a></li>
+      <li><a href="http://localhost/market/about.php"><h3>حول</h3></a></li>
+      
+     </ul>
+      <form class="search-form" >
+     
+     <input type="text" placeholder="بحث">
+     <button>بحث</button>
+     
+ </form>
+ 
+      
+    </nav>
+     <div class="boxs">
+        <center>
+           <div class="box">
+    
+       <h2>حول</h2>
+       <img style="width:400px;height: 250px;border-radius: 5px 5px 5px 5px;" src="img/ab.png">
+       <h5>المكتبة هي عبارة عن مكتبة الاكترونية لتحميل كتب لغات البرمجة الاكثر شيوعا</h5>
+       <h5>لتعليم البرمجة</h5>
+
+        </center>
+        </div>
+     </div>
+    
+       
+    
+
+    <div class="footer">
+      <div class="news">
+        <ul>
+            <li><a href="#">رواد التكنولوجيا تفتح دورة لتصميم</a></li>
+            <li><a href="#">عروض جديد للزوار الكرام</a></li>
+            <li><a href="#">عروض خاصة بمناسبة الاعياد</a></li>
+            <li><a href="#">اصتضافات مجانية</a></li>
+            <li><a href="#">تدريبة لتعلم اساليب البرمجة</a></li>
+        </ul>
+      </div>
+      <div class="link">
+          رواد التكنولوجيا سارع الطار
+          <br/>
+          0991309993 : التلفون
+          <br/>
+          00967714490030 : تلفاكس
+          <br/>
+          البريد الاكتروني : en_radwad50@yahoo.com
+          <br/>
+          او عبر الصفحات الاجتماعية 
+          <br/>
+          <a href="https:www.facebook.com"><img src="img/face.png"></a>
+          <a href="https:www.youtube.com"><img src="img/you.png"></a>
+          <a href="https:www.twiter.com"><img src="img/twt.png"></a>
+          <br/>
+      </div>
+      <div class="soc">
+          <a href="#">لمزيد من المزاضيع</a>
+          <a href="#">ملفات تعريف الارطبات</a>
+          <img  src="img/img5.png">
+          <img src="img/img3.png">
+      </div>
+    </div>
+    
+
+</body>    
+</html>
